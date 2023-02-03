46ffb6fd9e46b822a790b7d56f4fdde08e906561
antonioRenato <antoniorpfs@gmail.com>
2023-02-03 19:14:08 -0300

    add Post

---
+++ src/components/Post.jsx
@@ -0,0 +1,48 @@
+import styles from "./Post.module.css"
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/antonioRenato.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Antonio Renato</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="03 de feveiro às 09:37" dateTime="">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala Galera 🖐</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifólio. É um projeto que
+          fiz no NLW Return.
+        </p>
+        <p>
+          👉 <a href="">dani.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
+          <a href="">#rocketseat</a>
+          {"  "}
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback...</strong>
+        <textarea placeholder="Deixe um comentário" />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+    </article>
+  )
+}
