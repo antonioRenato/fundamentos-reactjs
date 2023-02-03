72e835c37b9d753a201c2dbb27e66bd775983e97
antonioRenato <antoniorpfs@gmail.com>
2023-02-02 21:32:45 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Header } from "./components/header"
+import { Post } from "./Post"
+
+import styles from "./App.module.css"
+
+import "./global.css"
+import { Sidebar } from "./components/Sidebar"
+
+export function App() {
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Antonio Renato" content="Testando o teste" />
+          <Post author="Julian Falk" content="Que maneiro" />
+        </main>
+      </div>
+    </div>
+  )
+}
