2828e0e4de6cf4647c3d5ded8a01084d9583520a
antonioRenato <antoniorpfs@gmail.com>
2023-02-05 10:07:07 -0300

    criado componente avatar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar"
 import { Comment } from "./comment"
 import styles from "./Post.module.css"
 
@@ -6,10 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://github.com/antonioRenato.png"
-          />
+          <Avatar src="https://github.com/antonioRenato.png" />
           <div className={styles.authorInfo}>
             <strong>Antonio Renato</strong>
             <span>Web Developer</span>
