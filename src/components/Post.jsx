22cdd35b17c3efbd850e7688b5e078278ec2cf0f
antonioRenato <antoniorpfs@gmail.com>
2023-02-08 20:21:13 -0300

    criado key para listas

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -50,10 +50,10 @@ export function Post({ author, publishAt, content }) {
       <div className={styles.content}>
         {content.map((line) => {
           if (line.type === "paragraph") {
-            return <p>{line.content}</p>
+            return <p key={line.content}>{line.content}</p>
           } else if (line.type === "link") {
             return (
-              <p>
+              <p key={line.content}>
                 <a href="#">{line.content}</a>
               </p>
             )
@@ -76,7 +76,7 @@ export function Post({ author, publishAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map((comment) => {
-          return <Comment content={comment} />
+          return <Comment key={comment} content={comment} />
         })}
       </div>
     </article>
